ba6d9898ce379d8b465ba5e7268fe87d054f21e7
Alex Lewis <alexlewis374@gmail.com>
2020-07-01 11:47:33 +0100

    G: 1 and 1, 1 - 1 returns an array with the sum and the answer 0

--- main/hello.js
+++ main/hello.js
@@ -5,9 +5,15 @@ export default class Calculator {
       let answer = parseInt(sum)
       return [`${sum}`, answer]
     }
-
+    
     let splitSum = sum.split(" ")
-    let answer = parseInt(splitSum[0]) + parseInt(splitSum[2])
-    return [`${sum}`, answer]
+    if (splitSum[1] === "+") {
+      let answer = parseInt(splitSum[0]) + parseInt(splitSum[2])
+      return [`${sum}`, answer]
+    } 
+    if (splitSum[1] === "-") {
+      let answer = parseInt(splitSum[0]) - parseInt(splitSum[2])
+      return [`${sum}`, answer]
+    }
   }
 }
