f818c5f3c4ca0b22db9b902e0b55ead2075dd053
Alex Lewis <alexlewis374@gmail.com>
2020-07-01 11:41:27 +0100

    Refactor: removed number1 and number2 objects and moved into one line expression

--- main/hello.js
+++ main/hello.js
@@ -1,12 +1,9 @@
 export default class Calculator {
   
   calculate(sum) {
-    
     if (sum.length > 1) {
       let splitSum = sum.split(" ")
-      const number1 = parseInt(splitSum[0])
-      const number2 = parseInt(splitSum[2])
-      let answer = number1 + number2
+      let answer = parseInt(splitSum[0]) + parseInt(splitSum[2])
       return [`${sum}`, answer]
     }
     let answer = parseInt(sum)
