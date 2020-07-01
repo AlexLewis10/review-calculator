e00826708aea4104eee206414e3f37503a6b34fd
Alex Lewis <alexlewis374@gmail.com>
2020-07-01 12:00:12 +0100

    G: 10, returns an array with 10 and 19

--- main/hello.js
+++ main/hello.js
@@ -1,12 +1,14 @@
 export default class Calculator {
   
   calculate(sum) {
-    if (sum.length < 2) {
+    let splitSum = sum.split(" ")
+    if (splitSum.length < 2) {
       let answer = parseInt(sum)
       return [`${sum}`, answer]
     }
+   
     
-    let splitSum = sum.split(" ")
+    
     if (splitSum[1] === "+") {
       let answer = parseInt(splitSum[0]) + parseInt(splitSum[2])
       return [`${sum}`, answer]
@@ -17,6 +19,10 @@ export default class Calculator {
     }
     if (splitSum[1] === "*") {
       let answer = parseInt(splitSum[0]) * parseInt(splitSum[2])
+      return [`${sum}`, answer]
+    }
+    if (splitSum[1] === "/") {
+      let answer = parseInt(splitSum[0]) / parseInt(splitSum[2])
       return [`${sum}`, answer]
     }
   }
--- main/hello.unit.test.js
+++ main/hello.unit.test.js
@@ -7,8 +7,14 @@ beforeEach(() => {
 })
 
 describe("1", () => {
-  it ('returns an array with 1 and 1', () => {
+  it('returns an array with 1 and 1', () => {
     expect(calculator.calculate("1")).toEqual(["1", 1])
+  })
+})
+
+describe('10', () => {
+  it('returns an array with 10 and 19', () => {
+    expect(calculator.calculate("10")).toEqual(["10", 10])
   })
 })
 
@@ -23,5 +29,9 @@ describe('1 and 1', () => {
 
   it('1 * 1 returns an array with the sum and the answer 0', () => {
     expect(calculator.calculate("1 * 1")).toEqual(["1 * 1", 1])
+  })
+
+  it('1 / 1 returns an array with the sum and the answer 1', () => {
+    expect(calculator.calculate("1 / 1")).toEqual(["1 / 1", 1])
   })
 })
